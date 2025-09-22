a7dc2af21330b647a08364ac53b9ed559881ea6c
0x-wajahat <m.wajahat.jt@gmail.com>
2025-09-22 22:10:29 +0500

    Add files via upload
    A C++ program to convert temperature from Celsius (input) to Fahrenheit.

---
+++ Temperature_conversion.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+                   int main(){
+                   float Celsius, Fahrenheit;
+                   cout<<"Enter temperature in Celsius";
+                   cin>> Celsius;
+                   Fahrenheit=(Celsius*9.0/5.0)+32;
+                   cout<<"Temperature in Fahrenheit="<<Fahrenheit<<endl;
+                   return 0;
+                   }
